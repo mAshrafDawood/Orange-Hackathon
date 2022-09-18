15f89865a60f4074d53bd5bd10060fe1f2c4f784
mAshrafDawood <ashraf.dawood2295@gmail.com>
2022-09-18 07:14:42 +0200

    Initial commit

---
+++ src/main/java/com/example/orangehackathon/OrangeHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.orangehackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/orangehackathon/OrangeHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orangehackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
