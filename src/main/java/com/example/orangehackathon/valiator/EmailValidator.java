75c5f6d40d37e9d9f1e5c7c1eb68d120cd7cf6d0
mAshrafDawood <ashraf.dawood2295@gmail.com>
2022-09-23 20:32:25 +0200

    Fixed a bug in email regex

--- src/main/java/com/example/orangehackathon/valiator/EmailValidator.java
+++ src/main/java/com/example/orangehackathon/valiator/EmailValidator.java
@@ -12,7 +12,7 @@ public class EmailValidator {
      * Consecutive dots are not allowed.
      * local part length must be less than or equal to 64 characters
      * */
-    private static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$";
+    private static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 
 
     public static boolean isValid(String email){
